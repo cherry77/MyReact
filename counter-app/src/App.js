5975b4f24dcf454c9819613156d26469a4e0a910
flora <15071285862@163.com>
2020-02-18 08:55:27 +0800

    how to pass data between components

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import NavBar from './components/navbar'
+import Counters from './components/counters'
+// import Movies from './components/movies'
 
-import Movies from './components/movies'
+class App extends Component {
+    state = {
+        counters: [
+            {id: 1, value: 0},
+            {id: 2, value: 0},
+            {id: 3, value: 0},
+            {id: 4, value: 0}
+        ]
+    };
 
-function App() {
-  return (
-    <main className="container">
-        <Movies />
-    </main>
-  );
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        // counters[0].value ++;//这样也可以改变state里的值，我们需要在现有的这个位置克隆一遍
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    };
+    handleDelete = id => {
+        const counters = this.state.counters.filter(c => c.id !== id);
+        this.setState({
+            counters
+        })
+    };
+    handleReset = () => {
+        const counters = this.state.counters.map(c =>{
+            c.value = 0;
+            return c;
+        });
+        this.setState({
+            counters
+        })
+    };
+    render(){
+        return (
+            <React.Fragment>
+                <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+                <main className="container">
+                    <Counters
+                        counters={this.state.counters}
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}
+                        onReset={this.handleReset}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
+
 }
 
 export default App;
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,50 +2,17 @@ import React, {Component, Fragment} from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 0},
-            {id: 2, value: 0},
-            {id: 3, value: 0},
-            {id: 4, value: 0}
-        ]
-    };
-
-    handleIncrement = (counter) => {
-        const counters = [...this.state.counters];
-        // counters[0].value ++;//这样也可以改变state里的值，我们需要在现有的这个位置克隆一遍
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({counters});
-    };
-    handleDelete = id => {
-        const counters = this.state.counters.filter(c => c.id !== id);
-        this.setState({
-            counters
-        })
-    };
-    handleReset = () => {
-        const counters = this.state.counters.map(c =>{
-            c.value = 0;
-            return c;
-        });
-        this.setState({
-            counters
-        })
-    };
-
     render() {
         return (
             <Fragment>
                 <button className="btn btn-primary btn-sm m-2"
-                        onClick={() => this.handleReset()}>Reset</button>
+                        onClick={() => this.props.onReset()}>Reset</button>
                 {
-                    this.state.counters.map((counter, index) => (
+                    this.props.counters.map((counter, index) => (
                             <Counter key={index}
                                      counter={counter}
-                                     onIncrement={() => this.handleIncrement(counter)}
-                                     onDelete={() => this.handleDelete(counter.id)}/>
+                                     onIncrement={() => this.props.onIncrement(counter)}
+                                     onDelete={() => this.props.onDelete(counter.id)}/>
                         )
                     )}
             </Fragment>
